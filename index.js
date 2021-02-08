407c5bf04f82390ed20b1069102c1e21c7ce9817
clecazzz <70854934+clecazzz@users.noreply.github.com>
2021-02-08 14:28:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const data = require("./data.json");
+
+app.get("/", (req, res) => {
+    res.json(data);
+});
+
+app.listen(3100, () => {
+    console.log("Server Started");
+});
